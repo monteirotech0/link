5cb375ca833bd0a49d82c89a11f8614f62d9f721
Rodrigo Monteiro do Nascimento <rodrigo.monteiro.nascimento@outlook.com>
2023-09-01 13:38:16 -0300

    a

---
+++ blog/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("posts.json")
+        .then(response => response.json())
+        .then(data => {
+            const blogPosts = document.getElementById("blog-posts");
+
+            data.forEach(post => {
+                const postDiv = document.createElement("div");
+                postDiv.classList.add("post");
+
+                const title = document.createElement("h2");
+                title.textContent = post.title;
+
+                const content = document.createElement("p");
+                content.textContent = post.content;
+
+                postDiv.appendChild(title);
+                postDiv.appendChild(content);
+                blogPosts.appendChild(postDiv);
+            });
+        })
+        .catch(error => {
+            console.error("Erro ao carregar os posts:", error);
+        });
+});
